316dc9a751158a703744019afdef8e7212f90252
Eric <pradier_eric@orange.fr>
2022-05-02 12:26:07 +0200

    add

--- app.js
+++ app.js
@@ -15,7 +15,18 @@ connection.connect((err) => {
 app.use(express.json());
 
 app.get("/api/movies", (req, res) => {
-  connection.query("SELECT * FROM movies", (err, result) => {
+  let sql = 'SELECT * FROM movies';
+  const sqlValues = [];
+  if (req.query.color) {
+    sql += ' WHERE color = ?';
+    sqlValues.push(req.query.color);
+  }
+  if (req.query.max_duration) {
+    sql += ' WHERE duration <= ?';
+    sqlValues.push(req.query.max_duration);
+  }
+    connection.query(sql, sqlValues, (err, result) => {
+  // connection.query("SELECT * FROM movies", (err, result) => {
     if (err) {
       console.error(err);
       res.status(500).send("Error retrieving data from database");
@@ -26,14 +37,58 @@ app.get("/api/movies", (req, res) => {
 });
 
 app.get("/api/users", (req, res) => {
-  connection.query("SELECT * FROM users", (err, result) => {
-    if (err) {
+  let sql = 'SELECT * FROM users';
+  const sqlValues = [];
+  if (req.query.language) {
+    sql += ' WHERE language = ?';
+    sqlValues.push(req.query.language);
+  }
+  //connection.query("SELECT * FROM users", (err, result) => {
+    connection.query(sql, sqlValues, (err, result) => {
+  if (err) {
       console.error(err);
       res.status(500).send("Error retrieving users from database");
     } else {
       res.json(result);
     }
   });
+});
+
+app.get("/api/movies/:id", (req, res) => {
+  const movieId = req.params.id;
+  connection.query(
+    "SELECT * FROM movies WHERE id = ?",
+    [movieId],
+    (err, results) => {
+      if (err) {
+        res.status(500).send("Error retrieving movie from database");
+      } else {
+        if (results.length){
+          //res.status(200).send(console.log("Movie found"));
+          res.status(200);
+          console.log(res.status)
+          res.json(results[0]);
+        } 
+        else res.status(404).send("Movie not found");
+      }
+    }
+  );
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+  connection.query(
+    "SELECT * FROM users WHERE id = ?",
+    [userId],
+    (err, results) => {
+      if (err) {
+        res.status(500).send("Error retrieving user from database");
+      } else {
+        if (results.length) res.json(results[0]);
+        else res.status(404).send("User not found");
+      }
+    }
+  );
 });
 
 app.post("/api/movies", (req, res) => {
@@ -84,6 +139,7 @@ app.put("/api/users/:id", (req, res) => {
     }
   );
 });
+
 
 app.put("/api/movies/:id", (req, res) => {
   const movieId = req.params.id;
